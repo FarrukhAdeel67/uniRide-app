e4d86c020bb385fdbdb3b283ec09a1e5dedeafaa
FarrukhAdeel67 <m.farrukhadeel@gmail.com>
2022-10-29 22:42:21 +0500

    some packages installed

--- uniride-api/config/index.js
+++ uniride-api/config/index.js
@@ -2,59 +2,59 @@ const convict = require("convict");
 
 // Define a schema
 var config = convict({
-  env: {
-    doc: "The application environment.",
-    format: ["production", "development", "test"],
-    default: "development",
-    env: "NODE_ENV",
-  },
-  ip: {
-    doc: "The IP address to bind.",
-    format: "ipaddress",
-    default: "127.0.0.1",
-    env: "IP_ADDRESS",
-  },
-  port: {
-    doc: "The port to bind.",
-    format: "port",
-    default: 8080,
-    env: "PORT",
-    arg: "port",
-  },
-  pass_secret: {
-    doc: "PASS secret key",
-    format: String,
-    default: "",
-    env: "PASS_SECRET",
-  },
-  db: {
-    host: {
-      doc: "Database host name/IP",
-      format: String,
-      default: "127.0.0.1",
+    env: {
+        doc: "The application environment.",
+        format: ["production", "development", "test"],
+        default: "development",
+        env: "NODE_ENV",
     },
-    name: {
-      doc: "Database name",
-      format: String,
-      default: "database_development",
+    ip: {
+        doc: "The IP address to bind.",
+        format: "ipaddress",
+        default: "127.0.0.1",
+        env: "IP_ADDRESS",
     },
-    username: {
-      doc: "db user",
-      format: String,
-      default: "root",
+    port: {
+        doc: "The port to bind.",
+        format: "port",
+        default: 8080,
+        env: "PORT",
+        arg: "port",
     },
-    password: {
-      doc: "db password",
-      format: "*",
-      default: null,
+    jwt_secret: {
+        doc: "jwt_secret_key",
+        format: String,
+        default: "",
+        env: "JWT_SEcRET",
     },
-  },
+    db: {
+        host: {
+            doc: "Database host name/IP",
+            format: String,
+            default: "127.0.0.1",
+        },
+        name: {
+            doc: "Database name",
+            format: String,
+            default: "database_development",
+        },
+        username: {
+            doc: "db user",
+            format: String,
+            default: "root",
+        },
+        password: {
+            doc: "db password",
+            format: "*",
+            default: null,
+        },
+    },
 });
 
 // Load environment dependent configuration
 let env = config.get("env");
 if (env === "development") {
-  config.loadFile(__dirname + "/environments/" + env + ".json");
+    config.loadFile(__dirname + "/environments/" + env + ".json");
 }
 
 // Perform validation
