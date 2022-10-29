55e19613c81a57b3355ead6f8437385bf0bf9549
FarrukhAdeel67 <m.farrukhadeel@gmail.com>
2022-10-29 22:47:39 +0500

    phoneNumber is converted to snake case

--- uniride-api/database/migrations/20210513175910-create-users-table-migration.js
+++ uniride-api/database/migrations/20210513175910-create-users-table-migration.js
@@ -4,42 +4,42 @@ const table = "users";
 const { DataTypes } = require("sequelize");
 
 module.exports = {
-  up: async function (queryInterface, DataTypes) {
-    await queryInterface.createTable(table, {
-      id: {
-        allowNull: false,
-        autoIncrement: true,
-        primaryKey: true,
-        type: DataTypes.INTEGER,
-      },
-      name: {
-        type: DataTypes.STRING,
-        allowNull: true,
-      },
-      phoneNumber: {
-        type: DataTypes.STRING,
-        allowNull: false,
-      },
-      password: {
-        allowNull: false,
-        type: DataTypes.STRING,
-      },
-      category: {
-        type: DataTypes.ENUM("Student", "Faculty", "Driver"),
-        allowNull: false,
-        defaultValue: "Student",
-      },
-      createdAt: {
-        allowNull: false,
-        type: DataTypes.INTEGER,
-      },
-      updatedAt: {
-        allowNull: false,
-        type: DataTypes.INTEGER,
-      },
-    });
-  },
-  down: async function (queryInterface) {
-    await queryInterface.dropTable(table);
-  },
+    up: async function(queryInterface, DataTypes) {
+        await queryInterface.createTable(table, {
+            id: {
+                allowNull: false,
+                autoIncrement: true,
+                primaryKey: true,
+                type: DataTypes.INTEGER,
+            },
+            name: {
+                type: DataTypes.STRING,
+                allowNull: true,
+            },
+            phone_number: {
+                type: DataTypes.STRING,
+                allowNull: false,
+            },
+            password: {
+                allowNull: false,
+                type: DataTypes.STRING,
+            },
+            category: {
+                type: DataTypes.ENUM("Student", "Faculty", "Driver"),
+                allowNull: false,
+                defaultValue: "Student",
+            },
+            createdAt: {
+                allowNull: false,
+                type: DataTypes.INTEGER,
+            },
+            updatedAt: {
+                allowNull: false,
+                type: DataTypes.INTEGER,
+            },
+        });
+    },
+    down: async function(queryInterface) {
+        await queryInterface.dropTable(table);
+    },
 };
