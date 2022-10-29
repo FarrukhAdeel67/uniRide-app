4d3bf72bfb2537ced01cf303622bc69470e63968
FarrukhAdeel67 <m.farrukhadeel@gmail.com>
2022-10-29 22:42:47 +0500

    Update user.js

--- uniride-api/controllers/user.js
+++ uniride-api/controllers/user.js
@@ -1,85 +1,72 @@
 const { Users, sequelize } = require("../models");
 const config = require("../config");
-const CryptoJS = require("crypto-js");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 module.exports = {
-  signUp: async (req, res) => {
-    const transaction = await sequelize.transaction();
-    try {
-      const { name, phoneNumber, password } = req.body;
-      const encryptingPassword = CryptoJS.AES.encrypt(
-        password,
-        config.get("pass_secret")
-      ).toString();
-      if (!name || !phoneNumber || !password) {
-        throw { status: 400, message: "Required fields cannot be empty." };
-      }
-      const phoneNumberFound = await Users.findOne({
-        where: {
-          phoneNumber: phoneNumber,
-        },
-        transaction,
-      });
-      if (phoneNumberFound) {
-        throw { status: 409, message: "Phone Number already exists." };
-      }
-      let user = await Users.create(
-        {
-          name: name,
-          phoneNumber: phoneNumber,
-          password: encryptingPassword,
-        },
-        {
-          transaction,
+    signUp: async(req, res) => {
+        const transaction = await sequelize.transaction();
+        try {
+            const { name, phoneNumber, password, category } = req.body;
+            if (!name || !phoneNumber || !password) {
+                throw { status: 400, message: "Required fields cannot be empty." };
+            }
+            const phoneNumberFound = await Users.findOne({
+                where: {
+                    phoneNumber: phoneNumber,
+                },
+                transaction,
+            });
+            if (phoneNumberFound) {
+                throw { status: 409, message: "Phone Number already exists." };
+            }
+            let user = await Users.create({
+                name: name,
+                phoneNumber: phoneNumber,
+                password,
+                category,
+            }, {
+                transaction,
+            });
+            user = user.toJSON();
+            delete user.password;
+            const token = jwt.sign({ user }, "jwt_secret");
+            await transaction.commit();
+            res.status(200).send({ user, token });
+        } catch (err) {
+            console.log(err);
+            await transaction.rollback();
+            return res
+                .status(err.status || 500)
+                .send(err.message || "Something went wrong...");
         }
-      );
-      await transaction.commit();
-      res.status(200).send({ user });
-    } catch (err) {
-      console.log(err);
-      await transaction.rollback();
-      return res
-        .status(err.status || 500)
-        .send(err.message || "Something went wrong...");
-    }
-  },
-
-  logIn: async (req, res) => {
-    const transaction = await sequelize.transaction();
-    try {
-      const { phoneNumber, password } = req.body;
-      if (!phoneNumber || !password) {
-        throw { status: 400, message: "Required feilds cannot be empty" };
-      }
-
-      let user = await Users.findOne(
-        {
-          where: { phoneNumber: phoneNumber },
-        },
-        transaction
-      );
-      if (!user) {
-        throw { status: 404, message: "User doesn't exist" };
-      }
-      const decryptPassword = CryptoJS.AES.decrypt(
-        user.password,
-        config.get("pass_secret")
-      ).toString(CryptoJS.enc.Utf8);
-      console.log(decryptPassword);
-      console.log(password);
-      if (decryptPassword !== password) {
-        throw { status: 400, message: "Wrong credentials!" };
-      }
-      user = user.toJSON();
-      delete user.password;
-      transaction.commit();
-      res.status(200).send({ user });
-    } catch (err) {
-      console.log(err);
-      transaction.rollback();
-      return res
-        .status(err.status || 500)
-        .send(err.message || "Something went wrong...");
-    }
-  },
+    },
+    logIn: async(req, res) => {
+        const transaction = await sequelize.transaction();
+        try {
+            let { user } = req;
+            console.log(user.id);
+            const { phoneNumber, password } = req.body;
+            if (!phoneNumber || !password) {
+                throw { status: 400, message: "Required feilds cannot be empty" };
+            }
+            if (user.phoneNumber !== phoneNumber) {
+                throw { status: 404, message: "wrong Phone Number" };
+            }
+            const hashedPassword = await bcrypt.compare(password, user.password);
+            if (!hashedPassword) {
+                throw { status: 400, message: "Wrong Password!" };
+            }
+            user = user.toJSON();
+            delete user.password;
+            transaction.commit();
+            res.status(200).send({ user });
+        } catch (err) {
+            console.log(err);
+            transaction.rollback();
+            return res
+                .status(err.status || 500)
+                .send(err.message || "Something went wrong...");
+        }
+    },
 };
